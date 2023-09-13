83ab0ef38b57425aa881c7356dbe7566b74ee301
Ian Micheal <59771322+ianmicheal@users.noreply.github.com>
2023-09-13 10:33:44 -0400

    Update memcpyskip.h

--- memcpyskip.h
+++ memcpyskip.h
@@ -1,4 +1,10 @@
-//memcpyskip.s  andrewk@napalm
+//memcpyskip.s  andrewk@napalm The license (in LICENSE) allows you a lot of freedom to use this code. 
+//Please don't do anything lame like replacing the text and releasing it as your
+//own... 
+
+
+//adk/napalm 2001
+//andrewk@napalm-x.com
 #ifndef __MEMCPYSKIP_H__
 #define __MEMCPYSKIP_H__
 
